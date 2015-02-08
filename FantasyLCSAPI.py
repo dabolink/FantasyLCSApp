fad49926c9288333587cfc02ccbfbb7838b64bb5
dabolink <dabolink@uvic.ca>
2015-02-07 22:05:13 -0800

    added teams by league

--- FantasyLCSAPI.py
+++ FantasyLCSAPI.py
@@ -4,7 +4,7 @@ import requests
 #print r.status_code
 #json = r.json()
 #print json
-html_string = "http://na.lolesports.com:80/api/{0}/{1}.json"
+html_string = "http://na.lolesports.com:80/api{0}/{1}.json{2}"
 
 
 class Player:
@@ -25,7 +25,7 @@ class Team_Player:
         self.isStarter = player["isStarter"]
 
 class Team:
-    def __init__(self, JSON, team_id):
+    def __init__(self, JSON, team_id=0):
         self.name = JSON["name"]
         self.roster = []
         for player in JSON["roster"]:
@@ -33,6 +33,19 @@ class Team:
 
         self.acroynm = JSON["acronym"]
         self.team_id = team_id
+
+
+class Game:
+    def __init__(self, JSON, game_id):
+        self.game_id = game_id
+        self.winner_id = JSON["winnerId"]
+        self.game_number = JSON["gameNumber"]
+        self.max_games = JSON["maxGames"]
+        self.game_length = JSON["gameLength"]
+        self.contestants = []
+        for team in JSON["contestants"]:
+            print JSON["contestants"][team]["id"]
+            self.contestants.append(get_team(JSON["contestants"][team]["id"]))
 
 
 
@@ -44,26 +57,54 @@ class Team:
 
 def get_player(player_id):
     # 11 = Dyrus
-    html = html_string.format("player", str(player_id))
+    html = html_string.format("/player", str(player_id), "")
     r = requests.get(html)
     #print r.status_code
     return Player(r.json(), player_id)
+
 
 #  /league/  #
 
 
 
 #  /Team/  #
+
+
 def get_team(team_id):
     # 1 = Team SoloMid
-    html = html_string.format("team", str(team_id))
+    html = html_string.format("/team", str(team_id), "")
     r = requests.get(html)
     return Team(r.json(), team_id)
+
+
+def get_teams_in_league(tournament_id):
+    teams = []
+    html = html_string.format("/tournament", tournament_id, "")
+    print html
+    r = requests.get(html)
+    JSON = r.json()
+    for team in JSON["contestants"]:
+        teams.append(get_team((JSON["contestants"][team]["id"])))
+    return teams
+
+#  /Game/  #
+
+
+def get_game(game_id):
+    html = html_string.format("/game", str(game_id), "")
+    r = requests.get(html)
+    return Game(r.json(), game_id)
+
+
 #  /Tests/  #
 
-print get_team(get_player(11).team_id).name
-
-for player in get_team(1).roster:
-    print player.name
-    print '\t' + player.role
-    print '\t' + player.id
+# print get_team(get_player(11).team_id).name
+#
+# for player in get_team(1).roster:
+#     print player.name
+#     print '\t' + player.role
+#     print '\t' + player.id
+#
+# print get_game(4544)
+# for team in get_teams_in_league(197):
+#     print team.name
